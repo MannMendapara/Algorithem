f0c34f5872cb2ad4e257079d0bcbe02ac601d3d8
Lalakiya Priyanka <priyankalalkiya192@gmail.com>
2023-08-26 10:26:05 +0530

    min anx max value from array

--- min_max.cpp
+++ min_max.cpp
@@ -7,9 +7,19 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     int arr[] = { 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n= sizeof(arr) / sizeof(arr[0]);
      sort(arr, arr + n);
      cout << arr[0] << " "; 
      cout << arr[n-1] << " ";
+
+// From my side 
+//    int temp=arr[0];
+//      for (int i = 0; i < n; i++)
+//      {
+//         if(temp>arr[i]){
+//             temp=arr[i];
+//         }
+//      }
+//      cout<<endl<<"mininum value is "<<temp;
     return 0;
 }
